49a3ed2bfed68f207402222a4f1316e1edd0a414
Lakshyaveer Turna <th.is.lakshyaveerturna@gmail.com>
2024-09-12 13:23:58 +0530

    c

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -28,11 +28,10 @@ const About = () => {
       </div>
       <div className='background-3' style={{justifyContent:'normal'}}>
         <div className='card'>
-          <h1 className='heading-1' style={{fontSize:'3.5rem', textDecoration:'underline'}}>About Our Founder</h1>
         <img className='mask-image' src={Founder}></img>
-        <h1 style={{textAlign:'center', opacity:'80%'}} className='heading-2'>Nimrit Kaur</h1>
-        <h2  className='heading-1' style={{marginBottom:'3rem', fontSize:'2rem'}}>Founder, Youth Cares</h2>
-           <p className='paragraph-2'>Nimrit Kaur, the 17-year-old founder of Youth Cares, is a passionate advocate for youth empowerment and community service. Driven by her vision to create positive change, Nimrit established Youth Cares to provide young people with opportunities to develop essential technological skills and engage in meaningful community work. A high school student with a deep interest in STEM and social impact, Nimrit has led numerous initiatives, from tech workshops to community drives, all aimed at fostering leadership, collaboration, and innovation among her peers. Her dedication to creating opportunities for others, combined with her strong leadership and forward-thinking mindset, has made her a respected figure in both educational and community circles. Through Youth Cares, Nimrit hopes to inspire and uplift the next generation, ensuring that even the unprivileged young people have the tools and confidence to pursue their goals and contribute to society</p>
+        <h1 style={{textAlign:'center', opacity:'80%', fontFamily:'cursive'}} className='heading-2'>Nimrit Kaur</h1>
+        <h2  className='heading-5' >Founder, Youth Cares</h2>
+           <p className='paragraph-2' style={{textAlign:'center'}}>Nimrit Kaur, the 17-year-old founder of Youth Cares, is a passionate advocate for youth empowerment and community service. Driven by her vision to create positive change, Nimrit established Youth Cares to provide young people with opportunities to develop essential technological skills and engage in meaningful community work. A high school student with a deep interest in STEM and social impact, Nimrit has led numerous initiatives, from tech workshops to community drives, all aimed at fostering leadership, collaboration, and innovation among her peers. Her dedication to creating opportunities for others, combined with her strong leadership and forward-thinking mindset, has made her a respected figure in both educational and community circles. Through Youth Cares, Nimrit hopes to inspire and uplift the next generation, ensuring that even the unprivileged young people have the tools and confidence to pursue their goals and contribute to society</p>
       </div>
       </div>
       </div>
