e0c776a675c6691ab3fb3a52da5149fb9ad9ac43
Lakshyaveer Turna <th.is.lakshyaveerturna@gmail.com>
2024-09-13 12:06:58 +0530

    c

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';  // Assuming you'll add some styles
 
-import youthCaresLogo from './../assets/youthcaresLogo.png';
 
 const Navbar = () => {
   return (
